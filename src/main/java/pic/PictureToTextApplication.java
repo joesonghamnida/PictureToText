2078e4825b5ebc91089e2113f3dc6a87c05cc239
Joe Morris <joe.morris.dev@gmail.com>
2017-01-24 02:20:55 -0600

    groundwork for frontend

---
+++ src/main/java/pic/PictureToTextApplication.java
@@ -0,0 +1,12 @@
+package pic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureToTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureToTextApplication.class, args);
+	}
+}
---
+++ src/main/java/pic/controllers/DisplayGetController.java
@@ -0,0 +1,7 @@
+package pic.controllers;
+
+/**
+ * Created by joe on 1/24/17.
+ */
+public class DisplayGetController {
+}
