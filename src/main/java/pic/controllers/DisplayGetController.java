f5204ac92c44d85b19eb9a3d93b3979f6605d4e1
Joe Morris <joe.morris.dev@gmail.com>
2017-02-06 17:28:13 -0600

    outlined controllers

--- src/main/java/pic/controllers/DisplayGetController.java
+++ src/main/java/pic/controllers/DisplayGetController.java
@@ -1,7 +1,24 @@
 package pic.controllers;
 
-/**
- * Created by joe on 1/24/17.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+import java.io.FileNotFoundException;
+
+@Controller
 public class DisplayGetController {
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home() {
+        return ("home");
+    }
+
+    @RequestMapping(path = "/displayResults", method = RequestMethod.GET)
+    public String displayResults(HttpSession session, Model model) throws FileNotFoundException {
+
+        return ("home");
+    }
 }
