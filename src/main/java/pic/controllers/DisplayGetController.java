9d33427ba4ac1ab8ed37c180a435aa5508474aa7
Joe Morris <joe.morris.dev@gmail.com>
2017-02-10 19:09:09 -0600

    fixed issue with retrieving photo

--- src/main/java/pic/controllers/DisplayGetController.java
+++ src/main/java/pic/controllers/DisplayGetController.java
@@ -5,8 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import pic.ImageUtil;
+import sun.awt.image.ToolkitImage;
 
 @Controller
 public class DisplayGetController {
@@ -17,7 +28,16 @@ public class DisplayGetController {
     }
 
     @RequestMapping(path = "/displayResults", method = RequestMethod.GET)
-    public String displayResults(HttpSession session, Model model) throws FileNotFoundException {
+    public String displayResults(HttpSession session, Model model) throws FileNotFoundException, IOException {
+
+        File file = (File) session.getAttribute("picture");
+
+        BufferedImage image = ImageIO.read(file);
+        
+        BufferedImage[][] blocks = ImageUtil.partitionImage(image, 8);
+        for(int i = 0; i < 100; i++){
+            System.out.println(blocks[i][i]);
+        }
 
         return ("home");
     }
