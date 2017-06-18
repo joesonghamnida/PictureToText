8244ab101b9d2c96445f186e33b12f02113a6c8c
Joe Morris <joe.morris.dev@gmail.com>
2017-06-18 17:32:15 -0400

    removed cruft

--- src/main/java/pic/controllers/DisplayGetController.java
+++ src/main/java/pic/controllers/DisplayGetController.java
@@ -39,6 +39,9 @@ public class DisplayGetController {
         ArrayList<Color> colors = new ArrayList<>();
         colors = PicDeconstruction.colorBreakdown(image, colors);
 
+
+        //TODO: why am I grouping this into blocks anyway?
+        //TODO: allow for adjustable block sizes - allows user to see what words occur
         ArrayList<ArrayList> blockGroups = new ArrayList<>();
         blockGroups = PicDeconstruction.groupIntoBlocks(colors, blockGroups);
 
@@ -48,86 +51,7 @@ public class DisplayGetController {
         HashMap<Integer, Integer> colorOccurrences = new HashMap<>();
         colorOccurrences = PicDeconstruction.removeColorDuplicates(colorOccurrences, blockValues);
         System.out.println("color occurrences size: "+colorOccurrences.size());
-
-        /*
-        System.out.println("Height: " + image.getHeight());
-        System.out.println("Width: " + image.getWidth());
-
-        for (int row = 0; row < image.getHeight(); row++) {
-            for (int column = 0; column < image.getWidth(); column++) {
-                //System.out.printf("Coordinates: %d %d\n", row, column);
-                Color color = new Color(image.getRGB(column, row));
-                colors.add(color);
-            }
-        }
-           */
-
-        //TODO: this will need to be refactored into it's own function
-        //TODO: why am I grouping this into blocks anyway?
-        //TODO: allow for adjustable block sizes - allows user to see what words occur
-        //have some value greater than color total cap
-        //group pixels into block, and store block in array list
-        /*
-        ArrayList<ArrayList> blockGroups = new ArrayList<>();
-        int block = 0;
-        while (block < colors.size()) {
-
-            ArrayList<Color> colorBlock = new ArrayList<>();
-
-            int limit = 0;
-
-            //check current block position to prevent OOB exceptions
-            if (block + 8 <= colors.size()) {
-                limit = 8;
-            } else {
-                limit = colors.size() - block;
-            }
-
-            //add # of pixels to block
-            for (int i = 0; i < limit; i++) {
-                colorBlock.add(colors.get(block + i));
-            }
-            blockGroups.add(colorBlock);
-            block += 8;
-        }
-        */
-
-        //break down list of blocks into individual block
-        //block the block down into colors
-        /*
-        ArrayList<Integer> blockValues = new ArrayList();
-        for(int i = 0; i < blockGroups.size(); i++){
-            ArrayList<Color> colorBlock = blockGroups.get(i);
-
-            int blockValue = 0;
-            for(int j = 0; j < colorBlock.size(); j ++){
-                Color color = colorBlock.get(j);
-                blockValue += color.getBlue();
-                blockValue += color.getGreen();
-                blockValue += color.getRed();
-                //this is commented out to lower upper range of value
-                //blockValue += color.getAlpha();
-            }
-            blockValues.add(blockValue);
-        }
-        System.out.println("block values size: "+blockValues.size());
-        */
-
-        //add up # of color values, remove duplicates
-        //key should be renamed to something clearer
-
-        /*for (Integer blockValue: blockValues) {
-
-            if(colorOccurrences.containsKey(blockValue)){
-                int occurrence = colorOccurrences.get(blockValue);
-              colorOccurrences.put(blockValue, occurrence + 1);
-            }
-            else{
-                colorOccurrences.put(blockValue, 1);
-            }
-        }*/
-
-
+        
         //read in text and break down into values
         ArrayList<String> text = new ArrayList<>();
         text = ReadText.readFile("text.txt");
